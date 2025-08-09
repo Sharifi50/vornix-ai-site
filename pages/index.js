724263852461352bd3a4a9d74b753b021467dab9
Sharifi50 <mustafa.sharifi2050@gmail.com>
2025-08-09 15:00:34 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to Vornix.ai 🚀</h1>
+      <p>Your AI-powered solutions start here.</p>
+    </div>
+  );
+}
